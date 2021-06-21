72f883ec8f112afd59850597330bcbb66a47a2f2
AbdelrahmanMostafaKamel <amk_elsayed_92@hotmail.com>
2021-06-21 18:07:21 +0200

    Initial commit:Simple Thread extends Class.

---
+++ src/SimpleThread.java
@@ -0,0 +1,22 @@
+public class SimpleThread extends Thread{
+    private String name;
+    public SimpleThread(String name){
+        this.name=name;
+    }
+    public SimpleThread(){
+        name=null;
+    }
+    @Override
+    public void run(){
+       for(int i=0;i<10;i++){
+           System.out.println(i+" "+name);
+           try{
+               sleep((int)(Math.random()*1000));
+           }
+           catch(InterruptedException e){
+               e.printStackTrace();
+           }
+       }
+       System.out.println("Done! "+name);
+    }
+}
---
+++ src/TestClass.java
@@ -0,0 +1,6 @@
+public class TestClass {
+    public static void main(String[] args){
+        SimpleThread th=new SimpleThread("Thread Object1");
+        th.start();
+    }
+}
